0f58c51ef27592c8f4245230af702daa5db275b2
Extreme Mission <37947057+extrememission@users.noreply.github.com>
2024-11-22 08:46:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ document.addEventListener('DOMContentLoaded', () => {
   const totalVerses = 31102;  // Total verses in the Bible
   let isTransitioning = false;  // Flag to check if a transition is in progress
 
+  // Mapping of book numbers to book names (defining the book names array)
+  const bookNames = [
+    'Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy',
+    'Joshua', 'Judges', 'Ruth', '1 Samuel', '2 Samuel',
+    '1 Kings', '2 Kings', '1 Chronicles', '2 Chronicles', 'Ezra',
+    'Nehemiah', 'Esther', 'Job', 'Psalms', 'Proverbs',
+    'Ecclesiastes', 'Song of Solomon', 'Isaiah', 'Jeremiah', 'Lamentations',
+    'Ezekiel', 'Daniel', 'Hosea', 'Joel', 'Amos',
+    'Obadiah', 'Jonah', 'Micah', 'Nahum', 'Habakkuk',
+    'Zephaniah', 'Haggai', 'Zechariah', 'Malachi', 'Matthew',
+    'Mark', 'Luke', 'John', 'Acts', 'Romans',
+    '1 Corinthians', '2 Corinthians', 'Galatians', 'Ephesians', 'Philippians',
+    'Colossians', '1 Thessalonians', '2 Thessalonians', '1 Timothy', '2 Timothy',
+    'Titus', 'Philemon', 'Hebrews', 'James', '1 Peter',
+    '2 Peter', '1 John', '2 John', '3 John', 'Jude',
+    'Revelation'
+  ];
+
   // Fetch Bible data from the local JSON file
   fetch('data/kjv.json')
     .then(response => response.json())
